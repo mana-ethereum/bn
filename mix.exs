6bef79f2f911885d22b615b4ef876bdc58c38130
Ayrat Badykov <ayratin555@gmail.com>
2022-02-13 11:59:31 +0200

    chore: bump deps

--- mix.exs
+++ mix.exs
@@ -25,9 +25,9 @@ defmodule Bn.MixProject do
 
   defp deps do
     [
-      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
-      {:credo, "~> 0.10.0", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 0.5", only: [:dev, :test], runtime: false}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.1", only: [:dev], runtime: false}
     ]
   end
 end
